9aa9b493204e7da4eaac349c26136340b47d2124
JustinBanzon <banzoj@rpi.edu>
2024-04-02 18:16:34 -0400

    fixed a bug in game_object.h

--- game/OutpostDetails.gd
+++ game/OutpostDetails.gd
@@ -17,14 +17,17 @@ func init(outpost, gameID):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	print("Started _process()")
 	if outpost.isFactory():
 		$VBoxContainer/Type.text = "Factory"
 		$VBoxContainer/HBoxContainer/Spacer1.show()
 		$VBoxContainer/HBoxContainer/Jump.show()
-		
+		print("outpost function run")
 		$VBoxContainer/HBoxContainer2/VBoxContainer/Production.text = "+" + str(outpost.getProductionAmount()) + " in " + Utilities.timeToStr(game.getNextProductionEvent(outpost.getID()) - game.getTime())
+		print("outpost function finished")
 	elif outpost.isMine():
 		$VBoxContainer/Type.text = "Mine"
+		print($VBoxContainer/Type.text)
 		#$VBoxContainer/HBoxContainer/Spacer1.show()
 		#$VBoxContainer/HBoxContainer/Jump.show()
 		
@@ -33,28 +36,31 @@ func _process(delta):
 		$VBoxContainer/Type.text = "Generator"
 		$VBoxContainer/HBoxContainer/Spacer1.hide()
 		$VBoxContainer/HBoxContainer/Jump.hide()
-		
+		print($VBoxContainer/Type.text)
 		$VBoxContainer/HBoxContainer2/VBoxContainer/Production.text = "+50 to electrical output"
-	
+	print("outpost.getID() run")
 	var owns = game.ownsObj(outpost.getID())
-	
+	print("outpost.canMine() run")
 	if owns and outpost.canMine():
 		$VBoxContainer/HBoxContainer/Mine.show()
 	else:
 		$VBoxContainer/HBoxContainer/Mine.hide()
-		
+	print("outpost.canUndo() run")
 	if owns and outpost.canUndo():
+		print("outpost.getOriginatingOrderType() run")
 		$VBoxContainer/HBoxContainer/Cancel.text = "Undo '" + outpost.getOriginatingOrderType() + "'"
 		$VBoxContainer/HBoxContainer/Spacer2.show()
 		$VBoxContainer/HBoxContainer/Cancel.show()
 	else:
 		$VBoxContainer/HBoxContainer/Spacer2.hide()
 		$VBoxContainer/HBoxContainer/Cancel.hide()
-	
+	print("outpost.getUnits() run")	
 	$VBoxContainer/HBoxContainer2/Units.text = str(outpost.getUnits())
-	
+	print("outpost.getColor() run")
 	get_parent().color = outpost.getColor()
+	print("outpost.getOwnerID() run")
 	get_parent().playerName = game.getPlayer(outpost.getOwnerID()).getName()
+	print("Finished _process()")
 
 
 func _on_jump_pressed():
