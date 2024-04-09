a28824c7e87d6cfcc2ec52146e429d9f4ae7c574
JustinBanzon <banzoj@rpi.edu>
2024-04-09 18:18:57 -0400

    Format timeline text
    formatted as hours : minutes : seconds

--- game/Utilities.gd
+++ game/Utilities.gd
@@ -12,16 +12,15 @@ func _process(delta):
 
 func numToStr(num: int, unit: String):
 	return str(num) + " " + unit + "s" if num != 1 else str(num) + " " + unit
-
+func twoDigit(num: int):
+	if num < 10:
+		return "0%d"%num
+	else:
+		return "%d"%num
 func timeToStr(time: int):
 	if time < 0: return timeToStr(abs(time)) + " ago"
-	if time > 365 * 24 * 60 * 60: return numToStr(int(time / (365 * 24 * 60 * 60)), "year")
-	if time > 4 * 7 * 24 * 60 * 60: return numToStr(int(time / (4 * 7 * 24 * 60 * 60)), "month")
-	if time > 7 * 24 * 60 * 60: return numToStr(int(time / (7 * 24 * 60 * 60)), "week")
-	if time > 24 * 60 * 60: return numToStr(int(time / (24 * 60 * 60)), "day")
-	if time > 60 * 60: return numToStr(int(time / (60 * 60)), "hour")
-	if time > 60: return numToStr(int(time / 60), "minute")
-	return numToStr(time, "second")
+	#Avoid things like 1:1:1
+	return "%d:%s:%s"%[(time/(60*60)),twoDigit((time/60)%60),twoDigit(time%60)]
 
 func timeToDateStr(t: int):
 	var time = Time.get_datetime_dict_from_unix_time(t + 60 * Time.get_time_zone_from_system().bias)
